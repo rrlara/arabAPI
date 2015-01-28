465caa92764126efebd79eebf5d47ad7e79a441f
rrlara <rrlara@gmail.com>
2015-01-28 10:30:58 -0800

    Added filename and body

--- main.js
+++ main.js
@@ -11,10 +11,29 @@ var congfigSettings = {
     "PATH": "_posts/blog"
 }
 
+var mockPost = {
+    layout: "blog",
+    published: "false",
+    title: "orale orale",
+    category: "blog",
+    language: "arabic",
+    comments: "true",
+    tags: ["tag1", "tag2"],
+    author: "IFPRI",
+    splash: "https://farm8.staticflickr.com/7498/16147010630_f59f6a7f00_o.jpg",
+    content: "Poverty in the Middle East and North Africa (MENA) is largely a rural phenomenon."
+}
+
+//var github = new Github({
+//    username: congfigSettings.USERNAME,
+//    password: congfigSettings.PASSWORD,
+//    auth: "basic",
+//    key: "ce7632587d588ff261c27f84232fbae07112d6df"
+//});
+
 var github = new Github({
-    username: congfigSettings.USERNAME,
-    password: congfigSettings.PASSWORD,
-    auth: "basic"
+    token: "ce7632587d588ff261c27f84232fbae07112d6df",
+    auth: "oauth"
 });
 
 var repo = github.getRepo(congfigSettings.ACCUSR, congfigSettings.REPO);
@@ -59,9 +78,13 @@ function readRawPostMD(path){
     });
 }
 
-function writeFileToGithub(){
+function writeFileToGithub(title, content){
 
-    repo.write(congfigSettings.BRANCH, congfigSettings.PATH + "/orale1.md", 'ArabSpatialisAwesome', 'test for write files', function(err) {
+    var postdata = buildPost(title, content);
+
+    console.log(postdata);
+
+    repo.write(congfigSettings.BRANCH, congfigSettings.PATH + "/" + postdata.filename, postdata.body, 'test for write files', function(err) {
 
         if (err){
             console.log("something went wroong");
@@ -73,3 +96,34 @@ function writeFileToGithub(){
     });
 
 }
+
+writeFileToGithub(mockPost.title, mockPost.content)
+
+function formatTtitle(text) {
+        return text.replace(/\s+/g, '-').toLowerCase();
+    }
+
+
+function JekyllBuilder() {
+    var YAML_FRONT_MATTER = '---\n' +
+        'layout: blog\n' +
+        'title: %title\n' +
+        '---\n\n';
+    return YAML_FRONT_MATTER;
+}
+
+function buildPost(title, data) {
+    filename = formatTtitle(title);
+    var getYMAL = JekyllBuilder();
+    yaml = getYMAL.replace('%title', title);
+
+    post = {
+        filename: filename + '.md',
+        body: yaml + data
+    };
+    console.log("[post] = ", post );
+
+    return post;
+}
+
+//buildPost(mockPost.title, mockPost.content);

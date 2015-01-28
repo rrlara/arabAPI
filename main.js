5c58a9ba2ee13265ffb327e1d26831fa2da8896d
rrlara <rrlara@gmail.com>
2015-01-28 15:14:16 -0800

    everything working. Formatting code for use

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
  * Created by renerodriguez on 1/27/15.
  */
 
+
 var congfigSettings = {
     "USERNAME": "Yifei.liu@cgiar.org",
     "PASSWORD": "arab2014",
@@ -11,34 +12,33 @@ var congfigSettings = {
     "PATH": "_posts/blog"
 }
 
-//var mockPost = {
-//    layout: "blog",
-//    published: "true",
-//    title: "english Tags2 blog",
-//    category: "blog",
-//    language: "english",
-//    comments: "true",
-//    tags: ["tag1", "tag2"],
-//    author: "Perry",
-//    affiliation: "IFPRI",
-//    splash: "https://farm8.staticflickr.com/7498/16147010630_f59f6a7f00_o.jpg",
-//    content: "Poverty in the Middle East and North Africa (MENA) is largely a rural phenomenon."
-//}
-
 var mockPost = {
     layout: "blog",
     published: "true",
-    title: "arabic3 blog",
+    title: "english Orale blog",
     category: "blog",
-    language: "arabic",
+    language: "english",
     comments: "true",
-    tags: ["التعاون الاقليمي","التعاون الاقليمي", "test", "test2"],
-    // tags: ["this worked"],
+    tags: ["tag1", "tag2"],
     author: "Perry",
     affiliation: "IFPRI",
-    splash: "https://www.usarab.com/images/arab-map.gif",
-    content: "إن التغذية الجيدة مهمة أيضا في أجندة التنمية المستدامة التي تتبلور في شكل أهداف التنمية المستدامة  قيد النقاش حاليا.  إن الحفاظ على التغذية الجيدة بشكل فطري يؤدي إلى تدفقها على امتداد دورة الحياة وعبر الأجيال. إن  التغذية تدعم قدرة الفرد على الصمود في وجه الصدمات و حالات عدم اليقين التي يسببها تغير المناخ والتقلبات السعرية المفرطة. كما تدعم التغذية جيلا من الابتكارات المطلوبة  لتلبية  التحدي المشترك لتحسين حياة الأجيال الحالية والقادمة بأساليب تحقق الاستدامة البيئية.قامت مجموعة من الدول والمنظمات والباحثين والأكاديميين، تحت قيادة المعهد الدولي لبحوث السياسات الغذائية، بإطلاق أول تقرير مفصل حول الصحة العالمية والتقدم القُطري في اتجاه  الحد من سوء التغذية في سائر أنحاء العالم.  يقدم [تقرير التغذية العالمي](http://www.ifpri.org/sites/default/files/publications/gnr14.pdf) (بالانجليزية) النمط العالمي إضافة إلى الأنماط القطرية فيما يتصل بالتغذية في كل من الدول الأعضاء بالأمم المتحدة وعددها 193، كما يتضمن تقدما محددا لكل دولة. ولقد كان التقرير محور المؤتمر الدولي الثاني للتغذية الذي عُقد في روما في الفترة من 19 -21 نوفمبر، ونظمته كلا من منظمة الأغذية  و الزراعة ( الفاو) ومنظمة الصحة العالمية.  إن تقرير التغذية العالمية هذا هو الأول في مجموعة من التقارير السنوية.  يرصد التقرير التقدم العالمي في تحسين وضع التغذية؛ ويحدد الاختناقات التي تستلزم التغيير؛ ويبرز الفرص المتوافرة للتصرف؛ ويساهم في تعزيز المساءلة في مجال التغذية. إن هذه المجموعة من التقارير هي نتاج التزام الموقعين في مؤتمر قمة التغذية من أجل النمو في عام 2013. وتقوم مجموعة واسعة النطاق من أصحاب المصلحة بدعم التقرير ويقدمه مجموعة مستقلة من الخبراء بشراكة مع عدد كبير من المساهمين الخارجيين."
+    splash: "https://farm8.staticflickr.com/7498/16147010630_f59f6a7f00_o.jpg",
+    content: "Poverty in the Middle East and North Africa (MENA) is largely a rural phenomenon."
 }
+
+//var mockPost = {
+//    layout: "blog",
+//    published: "true",
+//    title: "arabic3 blog",
+//    category: "blog",
+//    language: "arabic",
+//    comments: "true",
+//    tags: ["التعاون الاقليمي","التعاون الاقليمي", "test", "test2"],
+//    author: "Perry",
+//    affiliation: "IFPRI",
+//    splash: "https://www.usarab.com/images/arab-map.gif",
+//    content: "إن التغذية الجيدة مهمة أيضا في أجندة التنمية المستدامة التي تتبلور في شكل أهداف التنمية المستدامة  قيد النقاش حاليا.  إن الحفاظ على التغذية الجيدة بشكل فطري يؤدي إلى تدفقها على امتداد دورة الحياة وعبر الأجيال. إن  التغذية تدعم قدرة الفرد على الصمود في وجه الصدمات و حالات عدم اليقين التي يسببها تغير المناخ والتقلبات السعرية المفرطة. كما تدعم التغذية جيلا من الابتكارات المطلوبة  لتلبية  التحدي المشترك لتحسين حياة الأجيال الحالية والقادمة بأساليب تحقق الاستدامة البيئية.قامت مجموعة من الدول والمنظمات والباحثين والأكاديميين، تحت قيادة المعهد الدولي لبحوث السياسات الغذائية، بإطلاق أول تقرير مفصل حول الصحة العالمية والتقدم القُطري في اتجاه  الحد من سوء التغذية في سائر أنحاء العالم.  يقدم [تقرير التغذية العالمي](http://www.ifpri.org/sites/default/files/publications/gnr14.pdf) (بالانجليزية) النمط العالمي إضافة إلى الأنماط القطرية فيما يتصل بالتغذية في كل من الدول الأعضاء بالأمم المتحدة وعددها 193، كما يتضمن تقدما محددا لكل دولة. ولقد كان التقرير محور المؤتمر الدولي الثاني للتغذية الذي عُقد في روما في الفترة من 19 -21 نوفمبر، ونظمته كلا من منظمة الأغذية  و الزراعة ( الفاو) ومنظمة الصحة العالمية.  إن تقرير التغذية العالمية هذا هو الأول في مجموعة من التقارير السنوية.  يرصد التقرير التقدم العالمي في تحسين وضع التغذية؛ ويحدد الاختناقات التي تستلزم التغيير؛ ويبرز الفرص المتوافرة للتصرف؛ ويساهم في تعزيز المساءلة في مجال التغذية. إن هذه المجموعة من التقارير هي نتاج التزام الموقعين في مؤتمر قمة التغذية من أجل النمو في عام 2013. وتقوم مجموعة واسعة النطاق من أصحاب المصلحة بدعم التقرير ويقدمه مجموعة مستقلة من الخبراء بشراكة مع عدد كبير من المساهمين الخارجيين."
+//}
 
 var github = new Github({
     token: githubCreds.token,
@@ -51,7 +51,9 @@ function init(){
 
     getListOfMDFiles();
 
+
 }
+
 
 function getListOfMDFiles() {
     repo.contents(congfigSettings.BRANCH, congfigSettings.PATH, function (err, contents) {
@@ -69,9 +71,7 @@ function getListOfMDFiles() {
 
 //Reads the RAW data from Github
 function getRawPostMD(shaID){
-
     repo.getBlob(shaID, function(err, data) {
-
 //        console.log("[data] = ", data);
     });
 
@@ -79,26 +79,21 @@ function getRawPostMD(shaID){
 
 //Reads the RAW file from Github
 function readRawPostMD(path){
-
     repo.read(congfigSettings.BRANCH, path, function(err, data) {
 //        console.log("[read] = ", data);
     });
 }
 
+
 function writeFileToGithub(title, content){
-
     var postdata = buildPost(title, content);
-
 //    console.log(postdata);
-
     repo.write(congfigSettings.BRANCH, congfigSettings.PATH + "/" + postdata.filename, postdata.body, 'test for write files', function(err) {
-
         if (err){
             console.log("something went wroong");
         }else{
             console.log("cool");
         }
-
 
     });
 
@@ -162,3 +157,5 @@ function buildPost(title, data) {
 
     return post;
 }
+
+
